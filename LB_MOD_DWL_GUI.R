b2fcebf1feda1e199c70afd601dea0e88104e627
lbusett <lbusett@gmail.com>
2014-07-11 17:53:58 +0200

    Commit 11 07

--- LB_MOD_DWL_GUI.R
+++ LB_MOD_DWL_GUI.R
@@ -1,67 +1,60 @@
-LB_MOD_DWL_GUI = function {
+LB_MOD_DWL_GUI = function (out_proj_list, mod_prod_list){
 
-  out_proj_list = c("Sinusoidal","UTM 32N","latlon WGS84",'Other')
-  mod_prod_list = c("MOD13/MYD13","MOD09/MYD09","MOD11/MYD11")
-
-
-  if (file.exists(file.path(MOD_DWL_Options$Previous_Dir,'MOD_DWL_Previous.RData'))) {load (file.path(MOD_DWL_Options$Previous_Dir,'MOD_DWL_Previous.RData'))}
-  else (opts =  list(modprod = '',
-        startdate = '',
-        enddate = '',
-        start_x_sel = '',
-        end_x_sel ='',
-        start_y_sel = '',
-        end_y_sel = '',
-        proj_sel = '',
-        format_sel = '',
-        repro_sel ='',
-        bbox = c('','','',''),
-        out_folder = '')
-
-
-
-          data.frame(MOD_Dir = '', Start_Year = 2001, End_Year = 2012, ReDown = 1, ReProc = 1,ReProcIm = 1,
-                                    Shape_File = '', CLC_File_00 = '', NKer = 200, Method = 1, SDVI = 1, SNDVI = 1, nodata_out = FRG_Options$No_Data_Out_Rast))
-
+  if (file.exists(file.path(getwd(),'Previous','MOD_DWL_Previous.RData'))) {load (file.path(getwd(),'Previous','MOD_DWL_Previous.RData'))
+  } else {opts =  list(modprod = '',
+        startdate = '2000-01-01',
+        enddate = '2015-12-31',
+        start_x_sel = 1,
+        end_x_sel =1,
+        start_y_sel = 1,
+        end_y_sel = 1,
+        proj_sel = 'Sinusoidal',
+        format_sel = 'GTiff',
+        repro_sel ='No',
+        bbox = c('100','','300',''),
+        out_folder = '')}
 
   library(gWidgets)
   options("guiToolkit"="RGtk2")
 
-  main_win <- gwindow("MODIS DOWNLOAD Tool", visible=TRUE,horizontal = FALSE)
+  main_win <- gbasicdialog(title = "Dialog", parent=NULL, do.buttons=FALSE)
+
   main_group = ggroup(container = main_win, horizontal = FALSE)
   prod_frame <- gframe(text ="MODIS Products",horizontal = TRUE, container=main_group)
-  prod_sel <- gcheckboxgroup(items = mod_prod_list, container=prod_frame, horizontal = T)
+  checked = rep(F, length(mod_prod_list)) ; checked[match(opts$modprod, mod_prod_list)] = T
+  prod_sel <- gcheckboxgroup(items = mod_prod_list, container=prod_frame, horizontal = T, checked = checked)
+
 
   dates_frame = gframe(text = 'Processing period', container = main_group, horizontal = TRUE)
   start_date_lab = glabel(text = 'Starting Date', container = dates_frame)
-  start_date_sel <- gcalendar(text = 'Select', container=dates_frame)
+  start_date_sel <- gcalendar( container=dates_frame, text = opts$startdate)
 
   end_date_lab = glabel(text = 'Ending Date', container = dates_frame)
-  end_date_sel <- gcalendar(text = 'Select', container=dates_frame)
+  end_date_sel <- gcalendar(container=dates_frame, text = opts$enddate)
 
-  tiles_group = gframe(text = 'Procesing extent', container = main_group, horizontal = FALSE)
+  tiles_group = gframe(text = 'Processing extent', container = main_group, horizontal = FALSE)
   x_group = ggroup(container = tiles_group, horizontal = TRUE)
   start_x_lab = glabel(text = 'h_start', container = x_group)
-  start_x_sel <- gdroplist(items = seq(1:35), text = 'Select', container=x_group)
+  start_x_sel <- gdroplist(items = seq(1:35), text = 'Select', container=x_group, selected = opts$start_x_sel)
   end_x_lab = glabel(text = 'h_end', container = x_group)
-  end_x_sel <- gdroplist(items = seq(1:35), text = 'Select', container=x_group)
+  end_x_sel <- gdroplist(items = seq(1:35), text = 'Select', container=x_group, selected = opts$end_x_sel)
 
   y_group = ggroup(container = tiles_group, horizontal = TRUE)
   start_y_lab = glabel(text = 'v_start', container = y_group)
-  start_y_sel <- gdroplist(items = seq(1:18), text = 'Select', container=y_group)
+  start_y_sel <- gdroplist(items = seq(1:18), text = 'Select', container=y_group, selected = opts$start_y_sel)
   end_y_lab = glabel(text = 'v_end', container = y_group)
-  end_y_sel <- gdroplist(items = seq(1:18), text = 'Select', container=y_group)
+  end_y_sel <- gdroplist(items = seq(1:18), text = 'Select', container=y_group, selected = opts$end_y_sel)
 
-  show_map = gbutton(text = 'Show Map', border = T,
+  show_map = gbutton(text = 'Show Tiles Map', border = T,
                      handler = function(h,....) {x11()
                                     plot(raster("D:/Documents/Source_Code/R/LB_MOD_DWL/LB_MOD_DWL/Accessoires/MODIS_Tiles.gif"))},
                      container =tiles_group )
 
   output_proj_frame = gframe(text = 'Output Projection and Extent', container = main_group)
   output_proj_group = ggroup (container = output_proj_frame, horizontal = FALSE)
-  proj_sel <- gcombobox(out_proj_list, container=output_proj_group)
+  proj_sel <- gcombobox(out_proj_list, container=output_proj_group, selected = match(opts$proj_sel, out_proj_list) )
 
-  output_ext_lab = glabel(text = 'Select Output Bounding Box (in output proj coordinates)', container = output_proj_group)
+  output_ext_lab = glabel(text = 'Select Output Bounding Box (in OUTPUT proj coordinates) - Leave Blank if no resize needed', container = output_proj_group)
 
   output_lab_group =  ggroup(horizontal = TRUE, container=output_proj_group, width = 200)
   output_xmin_lab = glabel('              Minimum X              ', container = output_lab_group, width = 40)
@@ -70,18 +63,18 @@ LB_MOD_DWL_GUI = function {
   output_ymax_lab = glabel('              Maximum Y              ', container = output_lab_group)
 
   output_ext_group =  ggroup(horizontal = TRUE, container=output_proj_group)
-  output_xmin_sel = gedit(initial.msg = "Minimum X", container = output_ext_group)
-  output_xmax_sel = gedit(text = "Maximum X", container = output_ext_group)
-  output_ymin_sel = gedit(text = "Minimum Y", container = output_ext_group)
-  output_ymax_sel = gedit(text = "Maximum Y", container = output_ext_group)
+  output_xmin_sel = gedit(text = opts$bbox[1], container = output_ext_group)
+  output_xmax_sel = gedit(text = opts$bbox[2], container = output_ext_group)
+  output_ymin_sel = gedit(text = opts$bbox[3], container = output_ext_group)
+  output_ymax_sel = gedit(text = opts$bbox[4], container = output_ext_group)
 
   options_frame = gframe(text = 'Processing Options', container = main_group)
   opt_group = ggroup(container = options_frame)
   format_lab = glabel(text = 'Output Format', container = opt_group)
-  format_sel <- gdroplist(items = c('ENVI','GTiff'), text = 'Select', container=opt_group)
+  format_sel <- gdroplist(items = c('ENVI','GTiff'), text = 'Select', container=opt_group, selected = match(opts$format_sel, c('ENVI','GTiff')))
 
   repro_lab = glabel(text = 'ReDownload Existing', container = opt_group)
-  repro_sel <- gradio(items = c('Yes','No'), text = 'Select', container=opt_group)
+  repro_sel <- gradio(items = c('Yes','No'), text = 'Select', container=opt_group, selected = match(opts$repro_sel, c('Yes','No')))
 
   outfold_frame = ggroup(text = "Output Folder", container=main_group)    			# Main group
   outfold_group = ggroup(horizontal = TRUE, container=outfold_frame)  				# Main group
@@ -98,8 +91,7 @@ LB_MOD_DWL_GUI = function {
   start_group = ggroup(container = start_frame, horizontal = TRUE)
   start_but = gbutton(text = 'Start', container = start_group, handler = function(button,...){
 
-    browser()
-  # Retrieve selected values
+      # Retrieve selected values
     opts$modprod = svalue(prod_sel)
     opts$startdate = svalue(start_date_sel)
     opts$enddate = svalue(end_date_sel)
@@ -114,9 +106,12 @@ LB_MOD_DWL_GUI = function {
     opts$bbox = (c(svalue(output_xmin_sel),svalue(output_xmax_sel),svalue(output_ymin_sel),svalue(output_ymax_sel)))
 
     opts$out_folder = svalue(outfold_sel)
-
+    dir.create(file.path(getwd(),'Previous'))
+    dispose(main_win)
+    save(opts, file = file.path(getwd(),'Previous','MOD_DWL_Previous.RData'))
+    opts<<- opts
 
   })
-
-
+visible(main_win, set=TRUE) ## show the selection GUI
+opts
 }
--- LB_MOD_DWL_MAIN.R
+++ LB_MOD_DWL_MAIN.R
@@ -1,5 +1,9 @@
-LB_MOD_DWL_Main = function() {
-  
-  
-  
-}
+# LB_MOD_DWL_Main = function() {
+
+  out_proj_list = c("Sinusoidal","UTM 32N","Latlon WGS84")
+  mod_prod_list = c("MOD13/MYD13","MOD09/MYD09","MOD11/MYD11")
+  LB_MOD_DWL_GUI(out_proj_list,mod_prod_list)
+  opts$modprod
+
+# }
+  a = gbasicdialog(title = "Dialog", widget, parent=NULL, do.buttons=TRUE, handler = NULL, action=NULL,  toolkit=guiToolkit())
