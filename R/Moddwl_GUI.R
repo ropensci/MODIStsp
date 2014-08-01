86ea7b6474ac57effdab6b7d22288aa2d011924d
lbusett <lbusett@gmail.com>
2014-08-01 10:57:16 +0200

    modified moddwl_GUi

--- R/Moddwl_GUI.R
+++ R/Moddwl_GUI.R
@@ -1,29 +1,77 @@
-Moddwl_GUI = function (Moddwl_opts){
+moddwl_GUI = function (moddwl_opts){
 
-  if (file.exists(Moddwl_opts$Previous_File)) {load (Moddwl_opts$Previous_File)
-  } else {opts =  list(modprod = '',
-                       start_day = 1,
-                       start_month = 11,
-                       start_year = 2003,
-                       end_day = 1,
-                       end_month = 1,
-                       end_year = 2000,
-                       start_x = 1,
-                       end_x =1,
-                       start_y = 1,
-                       end_y = 1,
-                       proj = 'Sinusoidal',
-                       format = 'GTiff',
-                       reprocess ='No',
-                       bbox = c('100','','300',''),
-                       out_folder = '')}
+  # Restore previous options file if existing, otherwise create a "dummy" one with default values
+  if (file.exists(moddwl_opts$previous_file)) {load(moddwl_opts$previous_file)
+  } else {moddwl_set_opts(previous_file = moddwl_opts$previous_file) ; load(moddwl_opts$previous_file)}
 
   main_win <- gbasicdialog(title = "Select Main Processing Oprions", parent=NULL, do.buttons=FALSE)
 
   main_group = ggroup(container = main_win, horizontal = FALSE)
   prod_frame <- gframe(text ="MODIS Products",horizontal = TRUE, container=main_group)
-  checked = rep(F, length(Moddwl_opts$mod_prod_list)) ; checked[match(opts$modprod, Moddwl_opts$mod_prod_list)] = T
-  prod_sel <- gcheckboxgroup(items = Moddwl_opts$mod_prod_list, container=prod_frame, horizontal = T, checked = checked)
+  checked = which(opts$mod_prod_list == opts$modprod)
+  pos_prod = which(names(opts$prod_opt_list) == opts$modprod)
+  prod_sel <- gdroplist(items = opts$mod_prod_list, container=prod_frame, horizontal = T, selected = checked)
+
+  temp_sel_bands <<- opts$prod_opt_list[[pos_prod]]$bandsel
+  temp_sel_bands_derived <<- opts$prod_opt_list[[pos_prod]]$bandsel_derived
+
+  band_sel = gbutton(text = 'Select Processing Bands', border = T,
+                     handler = function(h,....) {
+
+                       pos_prod = which(names(opts$prod_opt_list) ==svalue(prod_sel))
+                       check_names = opts$prod_opt_list[[pos_prod]]$bandnames
+                       check_sel = temp_sel_bands
+
+                       selgroup = gbasicdialog(title = "Select Processing Bands", parent=NULL, do.buttons=F, width = 500, horizontal = T)
+                       #                             cfake = gtext(text = '', editable = F, width = 700, container = selgroup)
+                       cbox_total = gframe(text = "Select Processing Bands                     ", container = selgroup, horizontal = T, width = 500)
+
+                       cbox= gframe(texrt = "Original    ", container = cbox_total, horizontal = T, width = 500)
+                       bands_sel = gcheckboxgroup(items = check_names, checked = as.logical(check_sel), container = cbox, use.table = F, width = 500)
+
+                       cbox_derived= gframe(text = "Derived      ", container = cbox_total, horizontal = FALSE, width = 500)
+
+                       check_names_derived = opts$prod_opt_list[[pos_prod]]$derived_bandnames
+                       check_sel_derived= temp_sel_bands_derived
+                       bands_sel_derived = gcheckboxgroup(items = check_names_derived, checked = as.logical(check_sel_derived), container = cbox_derived, use.table = F, width = 500)
+
+                       start_group_2 = ggroup(container = selgroup, horizontal = TRUE)
+                       start_but_2 = gbutton(text = 'Start', container = start_group_2, handler = function(button,...){
+#                          browser()
+                         # Retrieve selected values
+#                          temp_sel_bands =
+#
+
+#                          opts$prod_opt_list[[pos_prod]]$bandsel = b
+
+                         a = which(opts$prod_opt_list[[pos_prod]]$bandnames %in% svalue (bands_sel))
+                         b = array(data = 0 , dim = length(opts$prod_opt_list[[pos_prod]]$bandsel))
+                         b[a] = 1
+                         temp_sel_bands <<- b
+
+                         temp_sel_bands_derived = svalue (bands_sel_derived)
+                         a = which(opts$prod_opt_list[[pos_prod]]$derived_bandnames %in%  svalue (bands_sel_derived))
+                         b = array(data = 0 , dim = length(opts$prod_opt_list[[pos_prod]]$bandsel_derived))
+                         b[a] = 1
+                        temp_sel_bands_derived <<- b
+#
+                         dispose(selgroup)
+
+                       })
+                       quit_but_2 = gbutton(text = 'Cancel', container = start_group_2, handler = function(button,...){
+                         temp_sel_bands <<- check_sel
+                         temp_sel_bands_derived <<- check_sel_derived
+                         dispose(selgroup)}
+                       )
+
+
+                       visible(selgroup, set=TRUE)
+
+                     },container =prod_frame)
+# browser()
+  sens_frame <- gframe(text ="Satellites",horizontal = TRUE, container=main_group)
+  sens_sel = gradio(items = c("Terra","Aqua", "Both"),selected = which(c("Terra","Aqua", "Both") == opts$sensor),
+                    container = sens_frame, horizontal = T)
 
 
   dates_frame = gframe(text = 'Processing period', container = main_group, horizontal = T)
@@ -52,14 +100,17 @@ Moddwl_GUI = function (Moddwl_opts){
 
   show_map = gbutton(text = 'Show Tiles Map', border = T,
                      handler = function(h,....) {x11()
-                                                 plot(raster(file.path(Moddwl_opts$Main_Dir, "/Accessoires/MODIS_Tiles.gif")))},
+                                                 plot(raster(file.path(moddwl_opts$main_dir, "/Accessoires/MODIS_Tiles.gif")))},
                      container =x_group )
 
   output_proj_frame = gframe(text = 'Output Projection and Extent', container = main_group)
   output_proj_group = ggroup (container = output_proj_frame, horizontal = FALSE)
-  proj_sel <- gcombobox(names(Moddwl_opts$out_proj_list), container=output_proj_group, selected = match(opts$proj, names(Moddwl_opts$out_proj_list)))
+  proj_sel <- gcombobox(names(opts$out_proj_list), container=output_proj_group, selected = match(opts$proj, names(opts$out_proj_list)))
 
-  output_ext_lab = glabel(text = 'Select Output Bounding Box (in OUTPUT proj coordinates) - Leave Blank if no resize needed', container = output_proj_group)
+  output_res_lab = glabel(text = 'Output Resolution (in OUTPUT proj measure units - Blank for Native Resolution)', container = output_proj_group)
+  output_res_sel = gedit(text = opts$out_res , container = output_proj_group, width = 15)
+
+  output_ext_lab = glabel(text = 'Output Bounding Box (in OUTPUT proj coordinates) - Leave Blank if no resize needed', container = output_proj_group)
 
   output_lab_group =  ggroup(horizontal = TRUE, container=output_proj_group, width = 200)
   output_xmin_lab = glabel('      Minimum X      ', container = output_lab_group, width = 15)
@@ -98,6 +149,7 @@ Moddwl_GUI = function (Moddwl_opts){
 
     # Retrieve selected values
     opts$modprod = svalue(prod_sel)
+    opts$sensor = sens_sel
     opts$start_day = svalue(start_day_sel)
     opts$start_month = svalue(start_month_sel)
     opts$start_year = svalue(start_year_sel)
@@ -113,9 +165,28 @@ Moddwl_GUI = function (Moddwl_opts){
     opts$proj = svalue(proj_sel)
     opts$format = svalue(format_sel)
     opts$reprocess = svalue(reprocess_sel)
-    opts$bbox = as.numeric((c(svalue(output_xmin_sel),svalue(output_xmax_sel),svalue(output_ymin_sel),svalue(output_ymax_sel))))
-
+    opts$bbox = ((c(svalue(output_xmin_sel),svalue(output_xmax_sel),svalue(output_ymin_sel),svalue(output_ymax_sel))))
+    opts$out_res = (svalue(output_res_sel))
     opts$out_folder = svalue(outfold_sel)
+
+    if (exists ('temp_sel_bands')) {
+
+#       a = which(opts$prod_opt_list[[pos_prod]]$bandnames %in% temp_sel_bands)
+#       b = array(data = 0 , dim = length(opts$prod_opt_list[[pos_prod]]$bandsel))
+#       b[a] = 1
+      opts$prod_opt_list[[pos_prod]]$bandsel = temp_sel_bands
+      rm(temp_sel_bands, envir = globalenv())
+    }
+
+    if (exists ('temp_sel_bands_derived')) {
+
+#       a = which(opts$prod_opt_list[[pos_prod]]$derived_bandnames %in% temp_sel_bands_derived)
+#       b = array(data = 0 , dim = length(opts$prod_opt_list[[pos_prod]]$bandsel_derived))
+#       b[a] = 1
+      opts$prod_opt_list[[pos_prod]]$derived_bandsel = temp_sel_bands_derived
+      rm(temp_sel_bands_derived, envir = globalenv())
+    }
+
 
     check = T
     # Check if dates, processing extent and tilews selection make sense
@@ -127,17 +198,24 @@ Moddwl_GUI = function (Moddwl_opts){
     n_bbox_compiled = length(which(is.finite(opts$bbox)))
 
     if (n_bbox_compiled == 4){
-    if ((opts$bbox[1] > opts$bbox[2]) | (opts$bbox[3] > opts$bbox[4])) {gmessage('Error in Selected Output extent', title = 'Warning') ; check = F}}
+      if ((opts$bbox[1] > opts$bbox[2]) | (opts$bbox[3] > opts$bbox[4])) {gmessage('Error in Selected Output extent', title = 'Warning') ; check = F}}
     if ((n_bbox_compiled < 4) & (n_bbox_compiled > 0 )) {gmessage('Error in Selected Output extent', title = 'Warning') ; check = F}
 
 
     if (check == T) {
       dir.create(file.path(getwd(),'Previous'))
-      save(opts, file = Moddwl_opts$Previous_File)
+      save(opts, file = moddwl_opts$previous_file)
+      assign("Quit", F, envir=globalenv())
       dispose(main_win)
     }
 
   })
+  quit_but = gbutton(text = 'Cancel', container = start_group, handler = function(button,...){
+    assign("Quit", T, envir=globalenv())
+    dispose(main_win)}
+  )
+
+
   visible(main_win, set=TRUE) ## show the selection GUI
 
 }
