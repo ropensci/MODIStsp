aa0e75ad27dd5f85422b99d135354c08b9b4d561
Luigi Ranghetti <ggranga@gmail.com>
2015-05-04 10:21:33 +0200

    Making bash launcher

---
+++ moddwl.sh
@@ -0,0 +1,8 @@
+Rscript ./Accessoires/RscriptEcho.R `pwd` $1 $2
+# This batch should be run with two arguments:
+# 1) a logical to start /not to start the gui;
+# 2) the path of the RData with the settings
+# (see Moddwl_Main.R for further details)
+
+# example of launcher:
+# ./moddwl.sh FALSE ~/R/workspace/WARM_synced_updates/Mod_dwl_settings/reg_it_opts.RData
