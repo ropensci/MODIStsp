40d0d996334be9c2be0f2b1e1a5bf58fd4c938e5
ggranga <ggranga@gmail.com>
2015-05-27 18:07:03 +0200

    [same as above]

---
+++ inst/ExtData/Batch/MODIStsp.bat
@@ -0,0 +1,5 @@
+SET Rscript_Path="C:\Progra~1\R\R-3.0.3\bin\x64\RScript"
+SET MODIStsp_Path="D:\Documents\Source_Code\R\LB_MOD_DWL\modistsp\modtsp_launcher.R"
+SET Options_File="D:\Documents\Source_Code\R\LB_MOD_DWL\Previous\Moddwl_Previous.RData"
+SET GUI="FALSE"
+%Rscript_Path% %MODIStsp_Path% %GUI% %Options_File%
---
+++ inst/ExtData/Batch/MODIStsp.sh
@@ -0,0 +1,6 @@
+MODIStsp_Path="~/R/x86_64-unknown-linux-gnu-library/3.1/MODIStsp/Extdata/Batch/MODIStsp_launcher.R"
+Options_File="~/R/x86_64-unknown-linux-gnu-library/3.1/MODIStsp/Previous/MODIStsp_Previous.RData"
+GUI="FALSE"
+
+
+Rscript $MODIStsp_Path $GUI $Options_File
