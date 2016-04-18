41e1912aa7e544578537c66583e7f82d0e498c31
lbusett <lbusett@gmail.com>
2016-04-19 01:30:33 +0200

    Updated vign.rmd, descriptiosn

--- R/MODIStsp.R
+++ R/MODIStsp.R
@@ -80,7 +80,7 @@ MODIStsp <- function(gui=TRUE, options_file=NULL, spatial_file_path=NULL, MODISt
 
   # On interactive execution, load Rgtk2
   if (gui) {
-    require(gWidgetsRGtk2)
+    requireNa(gWidgetsRGtk2)
     options("guiToolkit" = "RGtk2")
   }
 
