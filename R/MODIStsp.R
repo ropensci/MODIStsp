6459aec9dd1141f5e0bf879a28a4eea237b81327
lbusett <lbusett@gmail.com>
2016-10-20 23:44:24 +0200

    Revert "temp"
    This reverts commit b9ffee2d8f65b6c80e102aede47362cbd425b87a.

--- R/MODIStsp.R
+++ R/MODIStsp.R
@@ -27,7 +27,6 @@
 #' @importFrom raster extent rasterOptions
 #' @importFrom tools file_path_sans_ext
 #' @importFrom utils memory.limit packageVersion
-#' @import gWidgets2RGtk2
 #' 
 #' @examples
 #' # Running the tool without any option will start the GUI with the default or last used 
