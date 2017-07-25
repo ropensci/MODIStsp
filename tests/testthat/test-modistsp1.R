a0ecdf2cab66f80b2c10f241eb7c1bf31a13e501
lbusett <lbusett@gmail.com>
2017-07-25 11:29:33 +0200

    first implementation of unit testing
    NOTE: Only working on my machine -

--- tests/testthat/test-modistsp1.R
+++ tests/testthat/test-modistsp1.R
@@ -2,8 +2,8 @@ context("MODIStsp_Processing")
 testthat::test_that("Test On MODIStsp", {
   # skip_on_cran()
   skip_on_travis()
-  expect_warning(MODIStsp(gui = FALSE, options_file = "D:/Temp/buttami/test_modis/test_MOD13A2.json"))
-  expect_warning(MODIStsp(gui = FALSE, options_file = "D:/Temp/buttami/test_modis/test_MOD13A2_ftp.json"))
-  expect_warning(MODIStsp(gui = FALSE, options_file = "D:/Temp/buttami/test_modis/test_MOD13A2_resized.json"))
+  MODIStsp(gui = FALSE, options_file = "/home/lb/Temp/buttami/test_modistsp/test_MOD13A2_http_full.json")
+  MODIStsp(gui = FALSE, options_file = "/home/lb/Temp/buttami/test_modistsp/test_MOD13A2_ftp_full.json")
+  MODIStsp(gui = FALSE, options_file = "/home/lb/Temp/buttami/test_modistsp/test_MOD13A2_http_resized.json")
   }
 )
