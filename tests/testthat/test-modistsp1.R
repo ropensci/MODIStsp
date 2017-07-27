62505bd396494e0a07f9ce10888e0406c201338e
ggranga <ggranga@gmail.com>
2017-07-27 14:36:20 +0200

    Change testthat using test mode

--- tests/testthat/test-modistsp1.R
+++ tests/testthat/test-modistsp1.R
@@ -1,9 +1,11 @@
 context("MODIStsp_Processing")
-testthat::test_that("Test On MODIStsp", {
-  # skip_on_cran()
-  skip_on_travis()
-  MODIStsp(gui = FALSE, options_file = "/home/lb/Temp/buttami/test_modistsp/test_MOD13A2_http_full.json")
-  MODIStsp(gui = FALSE, options_file = "/home/lb/Temp/buttami/test_modistsp/test_MOD13A2_ftp_full.json")
-  MODIStsp(gui = FALSE, options_file = "/home/lb/Temp/buttami/test_modistsp/test_MOD13A2_http_resized.json")
+testthat::test_that(
+  "Tests on MODIStsp", {
+    # skip_on_cran()
+    skip_on_travis()
+    for (n in c(1:3,5)) { # test 4 (http) can not be runned in non-interactive mode
+      cat("\nTest ",n,"...\n", sep="")
+      MODIStsp(test=n)
+    }
   }
 )
