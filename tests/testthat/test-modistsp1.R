16dc9c5ac5a654bcaf8c702c5c1d6fb3c3bb522e
lbusett <lbusett@gmail.com>
2017-07-31 08:27:18 +0200

    change in examples file

--- tests/testthat/test-modistsp1.R
+++ tests/testthat/test-modistsp1.R
@@ -1,11 +1,38 @@
 context("MODIStsp_Processing")
 testthat::test_that(
   "Tests on MODIStsp", {
+    
+    library(testthat)
     # skip_on_cran()
     skip_on_travis()
-    for (n in c(1:3,5)) { # test 4 (http) can not be runned in non-interactive mode
-      cat("\nTest ",n,"...\n", sep="")
-      MODIStsp(test=n)
-    }
+    
+    MODIStsp()
+    
+    print("test-http")
+    MODIStsp(gui = FALSE, options_file = system.file("ExtData/test_http.json", package = "MODIStsp")) 
+    
+    # What is this test ? 
+    print("test1")
+    MODIStsp(test = 1)   
+    
+    # What is this test ? 
+    print("test2")
+    MODIStsp(test = 2)
+    
+    # What is this test ? 
+    print("test3")
+    expect_warning(MODIStsp(test = 3))
+    
+    # What is this test ? 
+    # MODIStsp(test = 4)
+    
+    # What is this test ? 
+    print("test5")
+    expect_warning(MODIStsp(test = 5))
+    
+    # for (n in c(1:3,5)) { # test 4 (http) can not be runned in non-interactive mode
+    #   cat("\nTest ",n,"...\n", sep = "")
+    #   MODIStsp(test = n)
+    # }
   }
 )
