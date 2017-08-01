c0e69def1825d6ae7bb5a1037340cda66c54fe4b
ggranga <ggranga@gmail.com>
2017-08-01 14:05:21 +0200

    Correction on test 6

--- tests/testthat/test-modistsp1.R
+++ tests/testthat/test-modistsp1.R
@@ -6,30 +6,27 @@ testthat::test_that(
     # skip_on_cran()
     skip_on_travis()
     
-    # MODIStsp()
-    # 
-    # print("test-http")
-    # MODIStsp(gui = FALSE, options_file = system.file("ExtData/test_http.json", package = "MODIStsp")) 
-    
     ### Test 1: test of the basic operations of MODIStsp. ###
-    #   The test downloads two bands and extract one quality indicator from a single locale LST product
-    #   (MOD11A2), without any additional preprocessing operation. Output files are in GeoTiff format.
+    #   The test downloads two bands and extract one quality indicator from a single locale 
+    #   LST product (MOD11A2, Newfoundland), without any additional preprocessing operation. 
+    #   Output files are in GeoTiff format.
     cat("Test 1: basic extraction of bands and quality indicators\n")
     MODIStsp(test = 1)
     MODIStest_check_md5(test = 1)
     
     ### Test 2: test of geometric routines. ###
-    #   The test works on the same local product of test 1, performing geometric operations (clipping,
-    #   resampling resolution to 1000m. Output files are in ENVI format.
+    #   The test works on the same local product of test 1, performing geometric operations
+    #   (clipping on the extent of Avalon peninsula and resampling resolution to 1000m. 
+    #   Output files are in ENVI format.
     cat("Test 2: geometric operations\n")
     MODIStsp(test = 2)
     MODIStest_check_md5(test = 2)
     
     ### Test 3: test of the creation of spectral indices and time series. ###
-    #   The test work on a clipped SR local product (MOD09A1), acreating two defined spectral indices
-    #   (NDVI and SAVI) and one custom index (GVMI). Geometric operations are performed like in
-    #   test 2 (with a resampling to 250m resolution using mode), and processing options for time 
-    #   series creation are applied.
+    #   The test works on a SR local product (MOD09A1) clipped on a small region (Barbellino, 
+    #   Orobie Alps) and creates two defined spectral indices (NDVI and SAVI) and one custom 
+    #   index (GVMI). Geometric operations are performed like in test 2 (with a resampling to 
+    #   250m resolution using mode), and processing options for time series creation are applied.
     #   Output files are in GeoTiff compressed format, with vrt time series.
     cat("Test 3: spectral indices and creation of time series\n")
     expect_warning(MODIStsp(test = 3))
@@ -37,7 +34,8 @@ testthat::test_that(
     
     ### Test 4: test of HTTP download (from NSIDC) with seasonal period. ###
     #   This test downloads two snow cover products (MYD10CM) of 1st August of years 2015 and 2016,
-    #   applying geometric operations (clipping and reprojecting).
+    #   applying geometric operations (clipping on Svalbard islands and reprojecting
+    #   in Arctic Polar Stereographic projection).
     #   Output files and time series are in ENVI format.
     #   FIXME the test requires valid USGS credentials, which are asked in 
     #   interactive mode. For this reason the test is excluded from testthat routines,
@@ -48,7 +46,8 @@ testthat::test_that(
     # MODIStest_check_md5(test = 4)
     
     ### Test 5: test of HTTP download (from USGS). ###
-    #   This test downloads an albedo product (MCD43A3), clipping and reprojecting it.
+    #   This test downloads an albedo product (MCD43A3), clipping (Cape Verde islands)
+    #   and reprojecting it (Cape Verde National CRS).
     #   Output files and time series are in ENVI format.
     #   FIXME the test requires valid USGS credentials, which are asked in 
     #   interactive mode. For this reason the test is excluded from testthat routines,
@@ -60,7 +59,7 @@ testthat::test_that(
     
     ### Test 6: test of FTP download and union of MODIS tiles ###
     #   This test download four LAI products (MCD15A2H) from FTP and merge them in a clipped
-    #   overlapping area. 
+    #   overlapping area (Minorca island). 
     #   After reprojection in geographic coordinates, output files are exported as GeoTiff
     #   (scaling output values) and vrt time series are created.
     cat("Test 6: FTP download and union of MODIS tiles\n")
