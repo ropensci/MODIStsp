838737d8970f87f0b8427ab22f6f01ba6dbe1423
lbusett <lbusett@gmail.com>
2017-08-10 15:19:46 +0200

    furthe improvements in MODIStsp_proc_indexes

--- R/MODIStsp_process_indexes.R
+++ R/MODIStsp_process_indexes.R
@@ -1,26 +1,27 @@
 #'  MODIStsp_process_indexes
 #' @description function used to compute spectral indexes, given the index formula
 #' @details the function parses the index formula to identify the required bands. On the basis
-#' of identified bands, it retrieves the reflectance bands required, gets the data into R raster
-#' objects, performs the computation and stores results in a GeoTiff or ENVI raster file
-#' @param out_filename basename of the file in to which save results
-#' @param formula string Index formula, as derived from XML file and stored in prod_opts 
-#' within previous_file
-#' @param bandnames string array of names of original HDF layer. Used to identify the 
-#' bands required for index computation
-#' @param nodata_out string array of nodata values of reflectance bands
-#' @param out_prod_folder strng output folder for the product used to retrieve filenames 
-#' of rasters of original bands to be used in computations
-#' @param indexes_nodata_out string nodata value for resulting raster
-#' @param file_prefix string used to retrieve filenames of rasters of original bands 
-#' to be used in computations
-#' @param yy string string used to retrieve filenames of rasters of original bands 
-#' to be used in computations
-#' @param DOY string used to retrieve filenames of rasters of original bands to be 
-#' used in computations
-#' @param out_format string used to retrieve filenames of rasters of original bands 
-#' to be used in computations
-#' @param scale_val string (Yes/No) if Yes, output values in are computed as float -1 - 1, 
+#'   of identified bands, it retrieves the reflectance bands required, gets the data into R raster
+#'   objects, performs the computation and stores results in a GeoTiff or ENVI raster file
+#' @param out_filename `character` basename of the file in to which save results
+#' @param formula `character` Index formula, as derived from XML file and stored in prod_opts 
+#'   within previous_file
+#' @param bandnames `character` array of names of original HDF layer. Used to identify the 
+#'   bands required for index computation
+#' @param nodata_out `character` array of NoData values of reflectance bands
+#' @param out_prod_folder `character` output folder for the product used to retrieve filenames 
+#'   of rasters of original bands to be used in computations
+#' @param indexes_nodata_out `character` NoData value for resulting raster
+#' @param file_prefix `character` used to retrieve filenames of rasters of original bands 
+#'   to be used in computations
+#' @param compress `character` compression option for GTiff files
+#' @param yy `character` year string used to retrieve filenames of rasters of original bands 
+#'   to be used in computations
+#' @param DOY `character` doy string used to retrieve filenames of rasters of original bands to be 
+#'   used in computations
+#' @param out_format `character` string used to retrieve filenames of rasters of original bands 
+#'   to be used in computations
+#' @param scale_val `character` (Yes/No) if Yes, output values in are computed as float -1 - 1, 
 #' otherwise integer -10000 - 10000
 #' @return NULL - new raster file saved in out_filename
 #'
@@ -39,11 +40,19 @@ MODIStsp_process_indexes <- function(out_filename, formula, bandnames,
   dir.create(dirname(out_filename),
              showWarnings = FALSE, recursive = TRUE)
   
-  # initialize the "fun_string" --> in the end, fun_string contains a complete
-  # function definition. Parsing it allows to create on the fly a function to
-  # compute the specific index required 
-  fun_string <- "index <- function("		 
-  # initialize the "stack_string" for the overlay function
+  #   _________________________________________________________________________
+  #   Create "fun_strin" and "stack_string". These are parsable character  ####
+  #   strings that can be parsed and evaluated to create on the fly the 
+  #   function to be applied to compute the index and a temporary rasterstack
+  #   containing required data. 
+  
+  # initialize the "fun_string" --> at the end, fun_string contains a complete
+  # function definition. 
+  fun_string <- "index <- function("
+  
+  # initialize the "stack_string" for the overlay function --> at the end, it 
+  # contains a parsable string for creating a rasterStack containing 
+  # the bands required to compute the index 
   stack_string <- "tmp_stack <- raster::stack("   
   
   for (band in seq(along = bandnames)) {
@@ -57,12 +66,9 @@ MODIStsp_process_indexes <- function(out_filename, formula, bandnames,
       # file name for the band, year, doy
       temp_file <- file.path(
         out_prod_folder, temp_bandname, 
-        paste0(file_prefix, "_", temp_bandname, "_", yy, "_", DOY)
+        paste0(file_prefix, "_", temp_bandname, "_", yy, "_", DOY,
+               ifelse(out_format == "GTiff", ".tif", ".dat"))
       )
-      
-      temp_file <- paste0(temp_file,
-                          ifelse(out_format == "GTiff", ".tif", ".dat"))
-      
       temp_raster <- raster(temp_file) 
       # assign NA value
       raster::NAvalue(temp_raster) <- as.numeric(nodata_out[band])
@@ -74,9 +80,8 @@ MODIStsp_process_indexes <- function(out_filename, formula, bandnames,
       stack_string  <- paste0(stack_string, temp_bandname, ", ")
     }
   }
-  
-  stack_string <- paste0(substr(stack_string, 1, nchar(stack_string) - 2), ")")
-  # Finalize the fun_string
+  # Finalize the fun_string: all the bands are in: now add the formula and 
+  # close parenthesis. 
   if (scale_val == "Yes") {
     # if scale_val, indices are written as float -1 - 1
     fun_string <- paste0(fun_string, "...)", "{", formula, "}")
@@ -86,10 +91,14 @@ MODIStsp_process_indexes <- function(out_filename, formula, bandnames,
     fun_string <- paste0(fun_string, "...)", "{round(10000*(", formula, "))}")
     dt  <- "INT2S"
   }
+  # Finalize the stack_string: all the bands are in: now close parenthesis. 
+  stack_string <- paste0(substr(stack_string, 1, nchar(stack_string) - 2), ")")
   
   #   __________________________________________________________________________
   #   compute the index, by calling raster::overlay over fun_string, using  ####
-  #   stack_string as input
+  #   stack_string as input. NOTE: The fact that the bands are used in the 
+  #   correct "order" is guaranteed by the fact that they are added in the 
+  #   same order in "fun_string" and in "stack_string" in the cycle above !
   
   raster::overlay(x         = eval(parse(text = stack_string)),
                   fun       = eval(parse(text = fun_string)), 
@@ -102,7 +111,7 @@ MODIStsp_process_indexes <- function(out_filename, formula, bandnames,
                   NAflag    = as.numeric(nodata_out),
                   overwrite = TRUE)
   
-  # IF "ENVI", write the nodata value in the header
+  # IF "ENVI", write the NoData value in the header
   if (out_format == "ENVI") { 
     # If output format is ENVI, add data ignore value to the header file
     fileConn_meta_hdr <- file(paste0(
