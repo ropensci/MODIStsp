b7e9958243df8f500ddc17cdc13cedac0f7600e2
lbusett <lbusett@gmail.com>
2020-08-30 10:49:22 +0200

    Add functions to easily retrieve product layers names

--- R/MODIStsp_get_prodlayers.R
+++ R/MODIStsp_get_prodlayers.R
@@ -7,17 +7,22 @@
 #' @param prodname character containing the code of the desired MODIS product.
 #'  NOTE: for products available separately for Terra and Aqua (e.g., MOD13Q1/MYD13Q1),
 #'  use the code M*D_code_ (e.g., M*D13Q1)
-#' @return list, containing the slots: `bandnames`, `quality_bandnames` and
-#'  `indexes_bandnames`
+#' @return list, containing the slots: `prodname`, `bandnames`, `quality_bandnames` and
+#'  `indexes_bandnames`, `band_fullnames`, `quality_fullnames`, `indexes_fullnames`
 #'
 #' @author Lorenzo Busetto, phD (2014-2020) \email{lbusett@@gmail.com}
 #' @note License: GPL 3.0
 #' @export
 #' @examples
 #'
+#' # Get layers of product M*13Q1 based on code
 #' MODIStsp_get_prodlayers("M*13Q1")
 #'
-#' MODIStsp_get_prodlayers("MCD43C4")
+#' # Get layers of product M*13Q1 based on full name
+#' MODIStsp_get_prodlayers("Vegetation Indexes_16Days_250m (M*D13Q1)")
+#'
+#' # Get indexes names of product M*13Q1 based on full name
+#' MODIStsp_get_prodlayers("MCD43C4")$indexes_bandnames
 #'
 #'
 MODIStsp_get_prodlayers <- function(prodname) {
@@ -42,14 +47,48 @@ MODIStsp_get_prodlayers <- function(prodname) {
   prodname <- names(selprod)
   selprod <- selprod[[1]][["6"]]
   bandnames         <- selprod[["bandnames"]]
+  band_fullnames    <- selprod[["band_fullnames"]]
   quality_bandnames <- selprod[["quality_bandnames"]]
+  quality_fullnames <- selprod[["quality_fullnames"]]
   indexes_bandnames <- selprod[["indexes_bandnames"]]
+  indexes_fullnames <- selprod[["indexes_fullnames"]]
+
+  cust_ind      <- jsonlite::read_json(system.file("ExtData/Previous/MODIStsp_indexes.json",
+                                                   package = "MODIStsp"))
+
+  if (length(cust_ind) == 1) {
+    cust_ind <- NULL
+  } else {
+    cust_ind  <- cust_ind[[prodname]][["6"]]
+  }
+
+  indexes_bandnames <- c(indexes_bandnames,
+                         cust_ind$indexes_bandnames)
+  indexes_fullnames <- c(indexes_fullnames,
+                         cust_ind$indexes_fullnames)
+
   prodnames   <- list(prodname          = prodname,
                       bandnames         = bandnames,
+                      bandfullnames     = band_fullnames,
                       quality_bandnames = quality_bandnames,
-                      indexes_bandnames = indexes_bandnames)
+                      quality_fullnames = quality_fullnames,
+                      indexes_bandnames = indexes_bandnames,
+                      indexes_fullnames = indexes_fullnames)
   prodnames
 }
+
+#' @title Retrieve the names of all available product
+#' @description
+#'	Function used to retrieve the names of available MODIS products
+#' @return character array of product names
+#'
+#' @author Lorenzo Busetto, phD (2014-2020) \email{lbusett@@gmail.com}
+#' @note License: GPL 3.0
+#' @export
+#' @examples
+#' # Get MODIStsp product names
+#' MODIStsp_get_prodnames()
+
 
 MODIStsp_get_prodnames <- function() {
   prod_opt_list <- load_prodopts()
