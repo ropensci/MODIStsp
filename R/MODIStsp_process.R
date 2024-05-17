0ce3d9e660b6be99d4833161fd22b5add951308c
pkautio <123114800+pkautio@users.noreply.github.com>
2024-05-17 15:46:08 +0300

    Update MODIStsp_process.R

--- R/MODIStsp_process.R
+++ R/MODIStsp_process.R
@@ -229,6 +229,10 @@ MODIStsp_process <- function(proc_opts,
   check_aria <- Sys.which("aria2c")
   if (check_aria == "") use_aria <- FALSE
 
+  #   __________________________________________________________________________
+  #   Fetch Bearer token for further authentication within functions ####
+
+  earthdata_token <- get_earthdata_token(user, password)
 
   #   __________________________________________________________________________
   #   Start Working.                                                        ####
